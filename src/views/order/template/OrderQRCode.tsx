96d9dad7af138ca98b23eae88952454b828c6388
Dannyrand208 <tuanthanh2082000@gmail.com>
2024-10-01 23:43:07 +1000

    code qr

--- src/views/order/template/OrderQRCode.tsx
+++ src/views/order/template/OrderQRCode.tsx
@@ -1,320 +1,186 @@
 'use client'
 import React from 'react'
 
+
 import {
-  Timeline,
-  TimelineItem,
-  TimelineSeparator,
-  TimelineConnector,
-  TimelineDot,
-  TimelineOppositeContent
+  Timeline, TimelineItem, TimelineSeparator, TimelineConnector,
+  TimelineDot, TimelineOppositeContent
 } from '@mui/lab'
 
-import { Grid, Button, Divider } from '@mui/material'
+import { Grid, Button, Divider, Typography, Box } from '@mui/material'
 
-const orderInfo = {
-  orderId: {
-    label: 'Order ID:',
-    value: '00000123456'
-  },
-  creationDate: {
-    label: 'Creation Date:',
-    value: '09/08/2024'
-  },
-  product: {
-    name: {
-      label: 'Product Name:',
-      value: 'Cheese Cake'
-    },
-    ingredients: {
-      label: 'Ingredients:',
-      value: [
-        'Unsweetened chocolate',
-        'pecans',
-        'sunflower seed',
-        'cocoa powder',
-        'lima bean',
-        'brandy',
-        'vegemite',
-        'sorghum',
-        'muffin',
-        'water'
-      ]
-    },
-    productionDate: {
-      label: 'Production Date:',
-      value: '09/06/2024'
-    },
-    expirationDate: {
-      label: 'Expiration Date:',
-      value: '09/06/2026'
-    }
-  }
-}
-
-const deliveryData = [
-  {
-    type: {
-      label: 'Type:',
-      value: 'Starting address'
-    },
-    address: {
-      label: 'Address:',
-      value: '114 S Columbus St'
-    },
-    mode: {
-      label: 'Mode of Transportation:',
-      value: 'Air',
-      color: 'info'
-    }
-  },
-  {
-    type: {
-      label: 'Type:',
-      value: 'Station 1'
-    },
-    address: {
-      label: 'Address:',
-      value: 'Rd. Santa Ana, Illinois 85486'
-    },
-    mode: {
-      label: 'Mode of Transportation:',
-      value: 'Ocean',
-      color: 'primary'
-    }
-  },
-  {
-    type: {
-      label: 'Type:',
-      value: 'Station 2'
-    },
-    address: {
-      label: 'Address:',
-      value: 'Rd. Santa Ana, Illinois 85486'
-    },
-    mode: {
-      label: 'Mode of Transportation:',
-      value: 'Truck',
-      color: 'secondary'
-    }
-  },
-  {
-    type: {
-      label: 'Type:',
-      value: 'Delivery address'
-    },
-    address: {
-      label: 'Address:',
-      value: '7604 Denton Hwy #200'
-    },
-    mode: {
-      label: null,
-      value: null,
-      color: null
-    }
-  }
-]
-
-const customerInfo = {
-  name: {
-    label: 'Customer Name:',
-    value: 'Company A'
-  },
-  phoneNumber: {
-    label: 'Phone Number:',
-    value: '0123456789'
-  },
-  journey: {
-    departure: {
-      label: 'Departure:',
-      value: '114 S Columbus St, Sunbury, Ohio'
-    },
-    arrival: {
-      label: 'Arrival:',
-      value: '7604 Denton Hwy #200'
-    },
-    estimatedTime: {
-      label: 'Estimated time:',
-      value: '3 days 10 hours'
-    }
-  },
-  priceUSD: {
-    label: 'Price (USD):',
-    value: '30,000'
-  }
-}
+import HomeAddress from '../../../@core/svg/HomeAddress.svg'
+import Boat from '../../../@core/svg/Boat.svg'
+import Truck from '../../../@core/svg/Truck.svg'
+import Airplane from '../../../@core/svg/AirPlane.svg'
+import { useOrderController } from '../controllers/qrcode.controller'
 
 const OrderManagementQRCode = () => {
+  const {
+    orderInfo,
+    deliveryData,
+    customerInfo,
+    handleGenerateQRCode,
+    handleSendQRCode,
+    handleCancel
+  } = useOrderController(); // Getting the state and handlers from the controller
+
+  const renderModeIcon = (mode: any) => {
+    switch (mode) {
+      case 'Air': return <Airplane className="w-5 h-5 mr-1 text-blue" />
+      case 'Ocean': return <Boat className="w-5 h-5 mr-1 text-blue" />
+      case 'Truck': return <Truck className="w-5 h-5 mr-1 text-blue" />
+      default: return null
+    }
+  }
+
   return (
-    <div className='container mx-auto p-4'>
-      <h1 className='mb-6 text-3xl font-semibold'>QR Code</h1>
+    <Box className="container mx-auto p-4">
+      <Typography variant="h3" className="mb-6 font-semibold">QR Code</Typography>
 
       {/* Main Layout */}
-      <Grid container spacing={4} alignItems='flex-start'>
+      <Grid container spacing={4}>
         {/* Left Side: Order Info */}
         <Grid item xs={12} md={6}>
-          <div className='p-6 bg-white rounded-lg shadow-md h-[459px]'>
-            <div className='flex justify-between items-center'>
-              <h3 className='text-xl font-bold'>
-                {orderInfo.orderId.label} <span className='font-normal'>{orderInfo.orderId.value}</span>
-              </h3>
-              <p className='text-lg font-bold'>
-                {orderInfo.creationDate.label} <span className='font-normal'>{orderInfo.creationDate.value}</span>
-              </p>
-            </div>
+          <Box className="p-6 bg-white rounded-lg shadow-md h-[459px]">
+            <Box className="flex justify-between items-center">
+              <Typography variant="h4" className="font-bold">
+                Order ID: <span className="font-normal">{orderInfo.orderId}</span>
+              </Typography>
+              <Typography variant="h5" className="font-bold">
+                Creation Date: <span className="font-normal">{orderInfo.creationDate}</span>
+              </Typography>
+            </Box>
 
-            <hr className='my-4' />
+            <Typography variant="h4" className="mt-5 font-bold">
+              Product Name: <span className="font-normal">{orderInfo.product.name}</span>
+            </Typography>
 
-            <h3 className='text-xl font-bold'>
-              {orderInfo.product.name.label} <span className='font-normal'>{orderInfo.product.name.value}</span>
-            </h3>
+            <Typography variant="h5" className="mt-4 font-bold">
+              Ingredients: <span className="font-normal">{orderInfo.product.ingredients.join(', ')}</span>
+            </Typography>
 
-            <p className='mt-4 font-bold'>
-              {orderInfo.product.ingredients.label}{' '}
-              <span className='font-normal'>{orderInfo.product.ingredients.value.join(', ')}</span>
-            </p>
+            <Typography variant="h5" className="mt-4 font-bold">
+              Production Date: <span className="font-normal">{orderInfo.product.productionDate}</span>
+            </Typography>
 
-            <p className='mt-4 font-bold'>
-              {orderInfo.product.productionDate.label}{' '}
-              <span className='font-normal'>{orderInfo.product.productionDate.value}</span>
-            </p>
-            <p className='mt-4 font-bold'>
-              {orderInfo.product.expirationDate.label}{' '}
-              <span className='font-normal'>{orderInfo.product.expirationDate.value}</span>
-            </p>
-          </div>
+            <Typography variant="h5" className="mt-4 font-bold">
+              Expiration Date: <span className="font-normal">{orderInfo.product.expirationDate}</span>
+            </Typography>
+          </Box>
         </Grid>
 
         {/* Right Side: QR Code */}
         <Grid item xs={12} md={6}>
-          <div className='p-6 bg-white rounded-lg shadow-md text-center'>
+          <Box className="p-6 bg-white rounded-lg shadow-md text-center">
             <img
-              src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/800px-QR_code_for_mobile_English_Wikipedia.svg.png'
-              alt='QR Code'
-              className='mx-auto w-[280px] h-[280px]'
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/800px-QR_code_for_mobile_English_Wikipedia.svg.png"
+              alt="QR Code"
+              className="mx-auto w-[280px] h-[280px]"
             />
-            <div className='flex justify-center mt-4'>
-              <Button variant='outlined' color='primary' size='large' className='mr-4'>
+            <Box className="flex justify-center mt-4">
+              <Button variant="outlined" color="primary" size="large" className="mr-4" onClick={handleGenerateQRCode}>
                 Generate QR code
               </Button>
-              <Button variant='contained' color='primary' size='large'>
+              <Button variant="contained" color="primary" size="large" onClick={handleSendQRCode}>
                 Send QR code
               </Button>
-            </div>
-            <Button variant='outlined' color='primary' size='large' className='mt-6'>
+            </Box>
+            <Button variant="outlined" color="primary" size="large" className="mt-6" onClick={handleCancel}>
               Cancel
             </Button>
-          </div>
+          </Box>
         </Grid>
       </Grid>
 
       {/* Route Details Section */}
-      <div className='mt-6 p-6 bg-white rounded-lg shadow-md'>
-        <div className='inline-flex items-center mb-6'>
-          <h5 className='text-xl font-bold mr-2'>Route detail:</h5>
-          <h5 className='text-xl'>DHL Group</h5>
-        </div>
+      <Box className="mt-6 p-6 bg-white rounded-lg shadow-md">
+        <Box className="inline-flex items-center mb-6">
+          <Typography variant="h4" className="font-bold mr-2">Route detail:</Typography>
+          <Typography variant="h4">DHL Group</Typography>
+        </Box>
 
-        {/* Route Information */}
         <Grid container spacing={4}>
           {/* Left Section: Customer Info */}
-          <Grid item xs={12} md={3}>
-            <div className='bg-white rounded-lg p-4'>
-              <p className='font-bold'>
-                {customerInfo.name.label} <span className='font-normal'>{customerInfo.name.value}</span>
-              </p>
-              <p className='font-bold'>
-                {customerInfo.phoneNumber.label} <span className='font-normal'>{customerInfo.phoneNumber.value}</span>
-              </p>
-              <p className='font-bold'>
-                {customerInfo.journey.departure.label}{' '}
-                <span className='font-normal'>{customerInfo.journey.departure.value}</span>
-              </p>
-              <p className='font-bold'>
-                {customerInfo.journey.arrival.label}{' '}
-                <span className='font-normal'>{customerInfo.journey.arrival.value}</span>
-              </p>
-            </div>
+          <Grid item xs={12} md={2.5}>
+            <Box className="bg-white rounded-lg p-4">
+              <Typography variant="h5" className="font-bold mb-2">
+                Customer Name: <span className="font-normal">{customerInfo.name}</span>
+              </Typography>
+              <Typography variant="h5" className="font-bold mb-2">
+                Phone Number: <span className="font-normal">{customerInfo.phoneNumber}</span>
+              </Typography>
+              <Typography variant="h5" className="font-bold mb-2">
+                Departure: <span className="font-normal">{customerInfo.journey.departure}</span>
+              </Typography>
+              <Typography variant="h5" className="font-bold mb-2">
+                Arrival: <span className="font-normal">{customerInfo.journey.arrival}</span>
+              </Typography>
+            </Box>
           </Grid>
 
-          {/* First Vertical Divider */}
-          <Divider orientation='vertical' flexItem sx={{ mx: 2 }} />
+          <Divider orientation="vertical" flexItem className="mx-4" />
 
-          {/* Middle Section: Address and Mode of Delivery (using Timeline) */}
+          {/* Middle Section: Address and Mode of Delivery */}
           <Grid item xs={12} md={7} container>
-            {/* Timeline on the far left */}
-            <Grid item>
-              <Timeline position='left'>
-                {deliveryData.map((item, index) => (
+            <Grid item xs={7}>
+              <Timeline position="left">
+                {deliveryData.map((item: { type: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; address: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined }, index: React.Key | null | undefined) => (
                   <TimelineItem key={index}>
                     <TimelineOppositeContent>
-                      <div>
-                        <strong>{item.type.value}</strong>
-                        <br />
-                        {item.address.value}
-                      </div>
+                      <Box className="text-lg">
+                        <strong>{item.type}</strong><br />{item.address}
+                      </Box>
                     </TimelineOppositeContent>
                     <TimelineSeparator>
-                      <TimelineDot color={item.mode.color || 'grey'}>
-                        {/* You can customize the icon based on the mode value */}
-                        <img
-                          src='https://icons.veryicon.com/png/o/miscellaneous/zhiutech-icon/house-34.png'
-                          alt='icon'
-                          style={{ width: '24px', height: '24px' }}
-                        />
+                      {/* Always show the home icon in the TimelineDot */}
+                      <TimelineDot color='grey'>
+                        <HomeAddress className="w-5 h-5 text-black" />
                       </TimelineDot>
-                      {index < deliveryData.length - 1 && <TimelineConnector />}
+                      {index < deliveryData.length - 1 && <TimelineConnector className="text-black" />}
                     </TimelineSeparator>
                   </TimelineItem>
                 ))}
               </Timeline>
             </Grid>
-            {/* Mode of delivery*/}
+
+            {/* Mode of delivery */}
             <Grid item xs>
-              <div className='ml-20 mt-1'>
+              <Box className="ml-4 mt-1">
                 {deliveryData.map((item, index) => (
-                  <div key={index} className='flex items-center mb-12'>
-                    <div className='flex items-center'>
-                      <span className='text-gray-600 mr-2'>{item.mode.label}</span>
-                      {item.mode.value ? (
-                        <div className='flex items-center'>
-                          <img src={`/${item.mode.value.toLowerCase()}.png`} alt='icon' className='w-5 h-5 mr-1' />
-                          <span className={`text-${item.mode.color}-600 font-medium leading-tight`}>
-                            {item.mode.value}
-                          </span>
-                        </div>
-                      ) : (
-                        ''
-                      )}
-                    </div>
-                  </div>
+                  <Box key={index} className="flex items-center mb-12">
+                    {item.mode && (
+                      <>
+                        <Typography variant="h5" className="text-gray-600 mr-2">Mode of Transportation:</Typography>
+                        {renderModeIcon(item.mode)}
+                        <Typography variant="h5" className="text-blue-600 font-bold leading-tight">
+                          {item.mode}
+                        </Typography>
+                      </>
+                    )}
+                  </Box>
                 ))}
-              </div>
+              </Box>
             </Grid>
           </Grid>
 
-          {/* Second Vertical Divider */}
-          <Divider orientation='vertical' flexItem sx={{ mx: 2 }} />
+          <Divider orientation="vertical" flexItem className="mx-4" />
 
           {/* Right Section: Estimated Time and Price */}
           <Grid item xs={3} md={1.6}>
-            <div className='text-center bg-white rounded-lg'>
-              <p className='text-sm font-bold'>
-                {customerInfo.journey.estimatedTime.label}
-                <br />
-                <span className='font-normal'>{customerInfo.journey.estimatedTime.value}</span>
-              </p>
-              <p className='text-sm font-bold'>
-                {customerInfo.priceUSD.label}
-                <br />
-                <span className='font-normal'>{customerInfo.priceUSD.value}</span>
-              </p>
-            </div>
+            <Box className="text-center bg-white rounded-lg">
+              <Typography variant="h5" className="font-bold mb-2">
+                Estimated time: <span className="font-normal">{customerInfo.journey.estimatedTime}</span>
+              </Typography>
+              <Typography variant="h5" className="font-bold">
+                Price (USD): <span className="font-normal">{customerInfo.priceUSD}</span>
+              </Typography>
+            </Box>
           </Grid>
         </Grid>
-      </div>
-    </div>
+      </Box>
+    </Box>
   )
 }
 
-export default OrderManagementQRCode
+export default OrderManagementQRCode;
