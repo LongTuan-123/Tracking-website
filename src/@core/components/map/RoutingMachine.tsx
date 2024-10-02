36e3f31f3058ebe26e4969689f8b478234e576f7
LongTuan-123 <85407964+LongTuan-123@users.noreply.github.com>
2024-10-02 22:31:46 +0700

    create map

--- src/@core/components/map/RoutingMachine.tsx
+++ src/@core/components/map/RoutingMachine.tsx
@@ -1,18 +1,29 @@
+'use client'
+
 import type { LatLng, MarkerOptions } from 'leaflet'
 import L from 'leaflet'
 import { createControlComponent } from '@react-leaflet/core'
-import 'leaflet-routing-machine'
 
-import LocationIcon from '../../svg/LocationIcon.svg'
-import useCommonStore from '@/store/common-store/useCommonStore'
+import 'leaflet-routing-machine'
 
 const LGet: any = L
 
 const useRoutineMachineLayer = () => {
-  const { waypoints } = useCommonStore()
+  const waypoints = [
+    L.latLng(21.028511, 105.778545),
+    L.latLng(21.030228, 105.790183),
+    L.latLng(21.036354, 105.825377),
+    L.latLng(21.029739, 105.837453), // Hang Dau Street coordinates
+    L.latLng(21.026282, 105.884655)
+  ]
+
+  const targetLocation = 'https://www.iconpacks.net/icons/2/free-location-icon-2955-thumb.png'
+
+  const stationLocation =
+    'https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes/128/home-circle-green-512.png'
 
   const instance = LGet.Routing.control({
-    waypoints,
+    waypoints: waypoints,
     lineOptions: {
       styles: [{ color: '#6FA1EC', weight: 4 }]
     },
@@ -21,11 +32,18 @@ const useRoutineMachineLayer = () => {
     routeWhileDragging: true,
     draggableWaypoints: false,
     fitSelectedRoutes: true,
-    showAlternatives: false,
+    showAlternatives: true,
     createMarker: (i: number, waypoint: { latLng: LatLng }): L.Marker => {
+      let iconUrl = stationLocation
+
+      if (i === 0 || i === waypoints.length - 1) {
+        iconUrl = targetLocation
+      }
+
       const markerOptions: MarkerOptions = {
         icon: L.icon({
-          iconUrl: LocationIcon,
+          iconRetinaUrl: iconUrl,
+          iconUrl: iconUrl,
           iconSize: [25, 41],
           iconAnchor: [12, 41],
           popupAnchor: [1, -34],
