68eaa6f12bb0152a35b13bc9b933c8f06f5986d6
Dannyrand208 <tuanthanh2082000@gmail.com>
2024-10-13 23:48:46 +1100

    feat(dashboard) : fix nhung loi con lai nhu grid 2 , toi uu code va phong cu

--- src/views/dashboard/controller/dashboard.controller.tsx
+++ src/views/dashboard/controller/dashboard.controller.tsx
@@ -1,69 +1,88 @@
 import { useState } from 'react';
 
+import Boat from '@svg/Boat.svg'
+
+import Airplane from '@svg/AirPlane.svg'
+
+import Truck from '@svg/Truck.svg'
+
 export const useDashboardController = () => {
   const [orderFilter, setOrderFilter] = useState<string>('This week');
   const [customerFilter, setCustomerFilter] = useState<string>('This month');
 
-  const orderData = [
-    { label: 'All Orders', value: 64 },
-    { label: 'Completed', value: 50 },
-    { label: 'Failed', value: 2 }
-  ];
+  const renderModeIcon = (mode:string) => {
+    switch (mode) {
+      case 'Air': return <Airplane className="w-5 h-5 mr-1 text-blue" />
+      case 'Ocean': return <Boat className="w-5 h-5 mr-1 text-blue" />
+      case 'Truck': return <Truck className="w-5 h-5 mr-1 text-blue" />
+      default: return null
+    }
+  }
 
-  const customerData = [
-    { label: 'Total Customers', value: 64 },
-    { label: 'New Customers', value: 16 },
-    { label: 'Scanned QR', value: 64 }
-  ];
+const customerOrderData = [
+  { section: 'Orders', data: [
+    { id: 1, label: 'All Orders', value: 64 },
+    { id: 2, label: 'Completed', value: 50 },
+    { id: 3, label: 'Failed', value: 2 }
+  ]},
+  { section: 'Customers', data: [
+    { id: 4, label: 'Total Customers', value: 64 },
+    { id: 5, label: 'New Customers', value: 16 },
+    { id: 6, label: 'Scanned QR', value: 64 }
+  ]}
+];
 
-  const tripsData = [
-    { label: 'Today', value: 64 },
-    { label: 'Preparing', value: 5 },
-    { label: 'Delivering', value: 15 },
-    { label: 'Completed', value: 44 }
-  ];
-
-  const onDeliveryData = [
-    { label: 'On-time', value: 64 },
-    { label: 'Delayed', value: 5 },
-    { label: 'Total Delay Time', value: '2000 mins' },
-    { label: 'Avg. Delay', value: '180 mins' }
-  ];
+ const TripDeliveryData = [
+  { section: 'Trips', data: [
+    { id: 1, label: 'Today', value: 64 },
+    { id: 2, label: 'Preparing', value: 5 },
+    { id: 3, label: 'Delivering', value: 15 },
+    { id: 4, label: 'Completed', value: 44 }
+  ]},
+  { section: 'On Delivery', data: [
+    { id: 5, label: 'On-time', value: 64 },
+    { id: 6, label: 'Delayed', value: 5 },
+    { id: 7, label: 'Total Delay Time', value: '2000 mins' },
+    { id: 8, label: 'Avg. Delay', value: '180 mins' }
+  ]}
+];
 
   const ordersDetail = [
   {
     label:'Order ID:',
     id: '0123456789',
     itemName: 'Cheese cake',
-    modeOfDelivery: 'Air',
+    type:'Air',
+    mode: 'Air',
     progress: '85%',
   },
   {
     label:'Order ID:',
     id: '9876543210',
     itemName: 'Cheese cake',
-    modeOfDelivery: 'Ground',
+    type:'Truck',
+    mode: 'Truck',
     progress: '60%',
   },
   {
     label:'Order ID:',
     id: '1928374650',
     itemName: 'Cheese cake',
-    modeOfDelivery: 'Sea',
+    type:'Ocean',
+    mode: 'Ocean',
     progress: '95%',
   }
 ];
 
-  
+
   return {
-    onDeliveryData,
-    tripsData,
+    TripDeliveryData,
     orderFilter,
     ordersDetail,
     customerFilter,
-    orderData,
-    customerData,
+    customerOrderData,
     setOrderFilter,
     setCustomerFilter,
+    renderModeIcon,
   };
 };
--- src/views/dashboard/template/Dashboard.tsx
+++ src/views/dashboard/template/Dashboard.tsx
@@ -2,103 +2,81 @@
 
 import React from 'react';
 
-import { Box, Grid, Card, Typography, Divider, Link } from '@mui/material';
+import Grid from '@mui/material/Grid2'
+
+import { Box, Card, Typography, Divider, Link } from '@mui/material';
+
 
 import OrderMap from '@/views/order/components/OrderMap';
 import WeekInput from '../component/InputWeek';
 import MonthInput from '../component/InputMonth';
 import { useDashboardController } from '../controller/dashboard.controller';
+import OrderInfo from '../component/OderInfo';
 
 
 const DashboardWithDropdown = () => {
   const {
+    TripDeliveryData,
     orderFilter,
     customerFilter,
-    tripsData,
     ordersDetail,
-    onDeliveryData,
-    orderData,
-    customerData,
+    customerOrderData,
     setOrderFilter,
-    setCustomerFilter
+    setCustomerFilter,
+    renderModeIcon
   } = useDashboardController();
 
   return (
     <Box className="p-4 bg-gray-100 min-h-screen">
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Card className="p-4">
-            <Box className="flex justify-between items-center">
-              <Typography variant="h4"  className="font-bold">Order</Typography>
-              <WeekInput weekFilter={orderFilter} setWeekFilter={setOrderFilter} />
-            </Box>
-            <Divider className="my-2" />
-            <Box className="flex justify-between">
-              {orderData.map((order, index) => (
-                <Box key={index}>
-                  <Typography variant="h6" className="mt-2">{order.label}</Typography>
-                  <Typography variant="h5" className="mt-3 font-bold">{order.value}</Typography>
+        {customerOrderData.map((section) => (
+          <Grid size={{ xs: 12, md: 6 }}key={section.section}>
+              <Card className="p-4">
+                <Box className="flex justify-between items-center">
+                  <Typography variant="h4" className="font-bold">{section.section}</Typography>
+                    {section.section === 'Orders' ? (
+                    <WeekInput weekFilter={orderFilter} setWeekFilter={setOrderFilter} />
+                  ) : (
+                    <MonthInput monthFilter={customerFilter} setMonthFilter={setCustomerFilter} />
+                  )}
                 </Box>
-              ))}
-            </Box>
-          </Card>
-        </Grid>
-
-
-        <Grid item xs={12} md={6}>
-          <Card className="p-4">
-            <Box className="flex justify-between items-center">
-                 <Typography variant="h4"  className="font-bold">Customer</Typography>
-              <MonthInput monthFilter={customerFilter} setMonthFilter={setCustomerFilter} />
-            </Box>
-            <Divider className="my-2" />
-            <Box className="flex justify-between">
-              {customerData.map((customer, index) => (
-                <Box key={index}>
-                  <Typography variant="h6" className="mt-2">{customer.label}</Typography>
-                  <Typography variant="h5" className="mt-3 font-bold">{customer.value}</Typography>
+                <Divider className="my-2" />
+                <Box className="flex justify-between">
+                  {section.data.map((order) => (
+                    <Box key={order.id}>
+                      <Typography variant="h6" className="mt-2">{order.label}</Typography>
+                      <Typography variant="h5" className="mt-3 font-bold">{order.value}</Typography>
+                    </Box>
+                  ))}
                 </Box>
-              ))}
-            </Box>
-          </Card>
-        </Grid>
+              </Card>
+          </Grid>
+))}
 
-
-        <Grid item xs={12}>
+        <Grid size={{ xs: 12 }}>
           <Card className="p-4">
             <Typography variant="h4"  className="font-bold">Active shipment</Typography>
             <Divider className="my-2" />
             <Grid container spacing={2}>
-              <Grid item xs={12} md={8.5}>
+              <Grid size={{ xs: 12, md: 8.5 }}>
                 <Box>
                   <Box className="h-96 bg-gray-200 rounded-lg mb-4">
                     <OrderMap/>
                   </Box>
                   {ordersDetail.map((order) => (
-                  <Box key={order.id} className="p-4 border rounded flex justify-between items-center">
+                  <Box key={order.id} className="p-4 border rounded flex justify-between items-center mb-2">
+                    <OrderInfo label={order.label} id={order.id} itemName={order.itemName}  />
                     <Box>
-                      <Typography variant="subtitle2">
-                        <strong>{order.label}</strong>
-                      </Typography>
-                      <Typography variant="body2">{order.id}</Typography>
-                      <Typography variant="body2">{order.itemName}</Typography>
+                     <Typography variant="h6" className="text-gray-600 mr-2">Mode of Transportation:</Typography>
+                        <Typography variant="h6" className="text-blue-600 font-bold leading-tight">
+                          {renderModeIcon(order.mode)}  {order.mode}
+                        </Typography>
                     </Box>
                     <Box>
-                      <Typography variant="subtitle2">
-                        <strong>Mode of Delivery:</strong>
-                      </Typography>
-                      <Typography variant="body2">
-                        <span role="img" aria-label="Air">
-                          ✈️
-                        </span>{' '}
-                        <strong>Air</strong>
-                      </Typography>
-                    </Box>
-                    <Box>
-                      <Typography variant="subtitle2">
+                      <Typography variant="h6">
                         <strong>Progress:</strong>
                       </Typography>
-                      <Typography variant="body2">85%</Typography>
+                      <Typography variant="h6">{order.progress}</Typography>
                     </Box>
                     <Link href="#" underline="always" color="primary" className="font-bold">
                       View detail
@@ -108,38 +86,24 @@ const DashboardWithDropdown = () => {
                 </Box>
               </Grid>
 
-              <Grid item xs={12} md={3.5}>
+              <Grid size={{ xs: 12, md: 3.5 }}>
                 <Box className="flex flex-col h-full">
-
-                  <Card className="flex-1 mb-2">
-                    <Box className="p-4">
-                      <Typography variant="h4"  className="font-bold">Trips</Typography>
-                      <Divider className="my-2" />
-                      <Box className="grid grid-cols-2 gap-4">
-                        {tripsData.map((trip, index) => (
-                          <Box key={index}>
-                            <Typography variant="h6" className="mt-2">{trip.label}</Typography>
-                            <Typography variant="h5" className="mt-3 font-bold">{trip.value}</Typography>
+               {TripDeliveryData.map((trip) => (
+                  <Card key={trip.section} className="flex-1 mb-2">
+                          <Box className="p-4">
+                            <Typography variant="h4"  className="font-bold">Trips</Typography>
+                            <Divider className="my-2" />
+                            <Box className="grid grid-cols-2 gap-4">
+                              {trip.data.map((trip) => (
+                                <Box key={trip.id}>
+                                  <Typography variant="h6" className="mt-2">{trip.label}</Typography>
+                                  <Typography variant="h5" className="mt-3 font-bold">{trip.value}</Typography>
+                                </Box>
+                              ))}
+                            </Box>
                           </Box>
-                        ))}
-                      </Box>
-                    </Box>
-                  </Card>
-
-                  <Card className="flex-1">
-                    <Box className="p-4">
-                      <Typography variant="h4"  className="font-bold">On Delivery</Typography>
-                      <Divider className="my-2" />
-                      <Box className="grid grid-cols-2 gap-4">
-                        {onDeliveryData.map((delivery, index) => (
-                          <Box key={index}>
-                            <Typography variant="h6" className="mt-2">{delivery.label}</Typography>
-                            <Typography variant="h5" className="mt-3 font-bold">{delivery.value}</Typography>
-                          </Box>
-                        ))}
-                      </Box>
-                    </Box>
-                  </Card>
+                        </Card>
+            ))}
                 </Box>
               </Grid>
             </Grid>
